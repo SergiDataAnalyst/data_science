b6e2a3fe90aee0fe06bdc46c3bf2e9bd46765be5
unknown <sergi.cmolina@gmail.com>
2022-12-04 21:31:28 +0100

    adding let it snow file

---
+++ let_it_snow.py
@@ -0,0 +1,65 @@
+import turtle
+import numpy as np
+import random
+
+def main(speed=0, bg_color="grey"):
+    # create Turtle object
+    turtle_screen = turtle.Screen()
+    myTurtle = turtle.Turtle()
+    
+    # set speed to 'fastest = 0'
+    myTurtle.speed(speed)
+    # change background color
+    turtle_screen.bgcolor(bg_color)
+  
+ 
+<<<<<<< HEAD
+    """TODO: define different colors here"""
+=======
+    """TODO: define difft colors """
+>>>>>>> feature_more_colors
+    
+    n_colors=1000
+    colors=["#"+''.join([random.choice('0123456789ABCDEF') for i in range(6)]) for j in range(n_colors)]
+
+    for _ in range(100):
+        # define some params
+        size = 18
+        pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
+        
+        
+<<<<<<< HEAD
+        """TODO: set snowflake color here (one of the colors defined above)"""
+=======
+        """TODO: set snowflake colors"""
+>>>>>>> feature_more_colors
+        
+        myTurtle.color(colors[np.random.randint(0,len(colors))])
+        
+        # Go to the start position of the snowflake
+        myTurtle.penup()
+        myTurtle.goto(pos[0], pos[1])
+        myTurtle.pendown()
+
+        # draw the snowflake
+        for _ in range(8):
+            snowflake_branch(size, myTurtle)
+            myTurtle.left(45)
+
+
+def snowflake_branch(size, myTurtle):
+    # This function draws one branch of the snowflake.
+    for _ in range(3):
+        for _ in range(3):
+            myTurtle.forward(size / 3)
+            myTurtle.backward(size / 3)
+            myTurtle.right(45)
+        myTurtle.left(90)
+        myTurtle.backward(size / 3)
+        myTurtle.left(45)
+    myTurtle.right(90)
+    myTurtle.forward(size)
+
+
+if __name__ == "__main__":
+    main()
