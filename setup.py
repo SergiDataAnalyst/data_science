b2c72ab234fef67cfb96821ea112c0d377fffb38
Sergi <117159450+SergiDataAnalyst@users.noreply.github.com>
2022-12-04 21:57:34 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find packages
+
+setup (
+    name="snowflake",
+    version="0.1",
+    author="Sergi",
+    author_email="sergi.molina@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy", "turtles"],
+    )
